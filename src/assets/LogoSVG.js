c4e2e28d711125b0f11e57b99004e55073d44f00
danielariawan93-debug <danielariawan93@gmail.com>
2026-04-18 15:37:11 +0700

    Add WPLogo and AJLogo components for logos

---
+++ src/assets/LogoSVG.js
@@ -0,0 +1,22 @@
+// src/assets/logoSVG.js
+import React from "react";
+
+export const WPLogo = ({ size = 24 }) => (
+  <img
+    src={`${process.env.PUBLIC_URL}/logo-wp.png`}
+    alt="Wealth Pulse"
+    width={size}
+    height={size}
+    style={{ objectFit: "contain", display: "block" }}
+  />
+);
+
+export const AJLogo = ({ size = 24 }) => (
+  <img
+    src={`${process.env.PUBLIC_URL}/logo-aj.png`}
+    alt="Artha Journey"
+    width={size}
+    height={size}
+    style={{ objectFit: "contain", display: "block" }}
+  />
+);
