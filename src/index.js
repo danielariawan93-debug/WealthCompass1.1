b83c5dfd2314c572665468b9abb636d1ed2df418
danielariawan93-debug <danielariawan93@gmail.com>
2026-04-24 16:29:20 +0700

    Change pathname check to use startsWith for routing

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import App from "./App";
 import QuickInputApp from "./QuickInputApp";
 
 const root = createRoot(document.getElementById("root"));
-const isQuickInput = window.location.pathname === "/quick-input";
-
+const isQuickInput = window.location.pathname.startsWith("/quick-input");
 root.render(
   <React.StrictMode>
     {isQuickInput ? <QuickInputApp /> : <App />}
